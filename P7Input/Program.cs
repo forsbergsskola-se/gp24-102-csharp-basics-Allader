f2cc1bcb4f702c3b25e86da9182ab128287f6db9
Draakoth <nikodemmajewski@gmail.com>
2024-09-16 14:00:23 +0200

    7-8 Done!
    Nice one

---
+++ P7Input/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What's your name?");
+
+string input = Console.ReadLine();
+
+Console.WriteLine("Hello, " + input);
