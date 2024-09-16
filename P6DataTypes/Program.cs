0383fc0152031e089f1ed284035b7088feb76d1f
Draakoth <nikodemmajewski@gmail.com>
2024-09-16 13:04:21 +0200

    6 done

--- P6DataTypes/Program.cs
+++ P6DataTypes/Program.cs
@@ -1,2 +1,11 @@
 ﻿string outputString = ("Hello World");
 Console.WriteLine(outputString);
+
+float decimalValue = (float)0.3;
+
+Console.WriteLine(decimalValue);
+
+bool isAwesome = true;
+
+Console.WriteLine(isAwesome);
+
